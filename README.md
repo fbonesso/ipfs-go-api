6e04602bfa2e1618e171519f8e32a771fbb8b934
Felipe Bonesso <fbonesso@gmail.com>
2016-10-04 16:19:03 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ipfs-go-api
-Go Lang API for InterPlanetary File System (IPFS) 
+Go Lang API for InterPlanetary File System (IPFS)
+
+## Usage
+`go run main.go`
+
+This will download the dependencies into $GOPATH
+
+## Methods
+`[POST] /ipfs/add`
+`[POST] /ipfs/execute/{command}`
+
+## Contribute
+Contributions are welcome! Please check out the [issues](https://github.com/fbonesso/ipfs-go-api/issues).
--- main.go
+++ main.go
@@ -1,89 +1,89 @@
 package main
 
 import (
-    "os"
-    "os/exec"
-    "io"
-    "fmt"
-    "log"
-    "net/http"
+  "os"
+  "os/exec"
+  "io"
+  "fmt"
+  "log"
+  "net/http"
 
-    "github.com/gorilla/mux"
-    "github.com/dchest/uniuri"
+  "github.com/gorilla/mux"
+  "github.com/dchest/uniuri"
 )
 
 func main() {
-    route := mux.NewRouter()
+  route := mux.NewRouter()
 
-    /*
-    * Add file to IPFS
-    */
-    route.HandleFunc("/ipfs/add", func(w http.ResponseWriter, r *http.Request) {
-        // CORS
-        w.Header().Set("Access-Control-Allow-Origin", "localhost")
+  /*
+  * Add file to IPFS
+  */
+  route.HandleFunc("/ipfs/add", func(w http.ResponseWriter, r *http.Request) {
+    // CORS
+    w.Header().Set("Access-Control-Allow-Origin", "localhost")
 
-        // Form
-        r.ParseMultipartForm(32 << 20)
+    // Form
+    r.ParseMultipartForm(32 << 20)
 
-        file, _, err := r.FormFile("file")
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-  			defer file.Close()
-  			if err != nil {
-  				http.Error(w, err.Error(), http.StatusInternalServerError)
-  				return
-  			}
+    file, _, err := r.FormFile("file")
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+    defer file.Close()
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
 
-        // UUID
-        uuid := uniuri.NewLen(10)
+    // UUID
+    uuid := uniuri.NewLen(10)
 
-        // Save
-  			dir, err := os.Create("tmp/" + uuid)
-  			defer dir.Close()
-  			if err != nil {
-  				http.Error(w, err.Error(), http.StatusInternalServerError)
-  				return
-  			}
+    // Save
+    dir, err := os.Create("tmp/" + uuid)
+    defer dir.Close()
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
 
-  			if _, err := io.Copy(dir, file); err != nil {
-  				return
-        }
+    if _, err := io.Copy(dir, file); err != nil {
+      return
+    }
 
-        // IPFS
-        var ipfs []byte
+    // IPFS
+    var ipfs []byte
 
-      	ipfs, _ = exec.Command("curl", []string{"-F", "file=@tmp/" + uuid, "http://localhost:5001/api/v0/add"}...).Output()
+    ipfs, _ = exec.Command("curl", []string{"-F", "file=@tmp/" + uuid, "http://localhost:5001/api/v0/add"}...).Output()
 
-        // Remove
-        os.Remove("tmp/" + uuid)
+    // Remove
+    os.Remove("tmp/" + uuid)
 
-        // Response
-        w.Write(ipfs)
-    })
+    // Response
+    w.Write(ipfs)
+  })
 
-    /*
-    * Execute command on IPFS
-    */
-    route.HandleFunc("/ipfs/execute/{command}", func(w http.ResponseWriter, r *http.Request) {
-        // CORS
-        w.Header().Set("Access-Control-Allow-Origin", "localhost")
+  /*
+  * Execute command on IPFS
+  */
+  route.HandleFunc("/ipfs/execute/{command}", func(w http.ResponseWriter, r *http.Request) {
+    // CORS
+    w.Header().Set("Access-Control-Allow-Origin", "localhost")
 
-        // Params
-        params := mux.Vars(r)
+    // Params
+    params := mux.Vars(r)
 
-        // Command
-        command := params["command"]
+    // Command
+    command := params["command"]
 
-        // IPFS
-        var ipfs []byte
+    // IPFS
+    var ipfs []byte
 
-      	ipfs, _ = exec.Command("ipfs", []string{command}...).Output()
+    ipfs, _ = exec.Command("ipfs", []string{command}...).Output()
 
-        // Response
-        w.Write(ipfs)
-    })
+    // Response
+    w.Write(ipfs)
+  })
 
-    log.Fatal(http.ListenAndServe(":8080", route))
+  log.Fatal(http.ListenAndServe(":8080", route))
 }
